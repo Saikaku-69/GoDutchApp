685237caa16eb8a1d66efbfd074330776729c04b
Saikaku-69 <grnmnsta@gmail.com>
2024-11-05 18:17:16 +0900

    initial Files
    First Commit

---
+++ GoDutchApp/GoDutchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoDutchAppApp.swift
+//  GoDutchApp
+//
+//  Created by cmStudent on 2024/11/05.
+//
+
+import SwiftUI
+
+@main
+struct GoDutchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
